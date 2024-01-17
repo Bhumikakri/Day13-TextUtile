29d26fc82a9966acab291080b0c70c6bdf1fe472
Bhumika Kumari <128302166+Bhumikakri@users.noreply.github.com>
2024-01-18 01:15:41 +0530

    About section is left

--- src/App.js
+++ src/App.js
@@ -1,33 +1,39 @@
+import React from 'react';
 import './App.css';
 import Main from './component/Main';
-import { createBrowserRouter , RouterProvider} from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './component/Home/Home';
 import AboutUs from './component/About/AboutUs';
 import Contact from './component/Contact/Contact';
+import { DarkModeProvider } from './component/DarkModeContext';
 
 function App() {
-  const routes=createBrowserRouter([
+  const routes = createBrowserRouter([
     {
-      path:'/',
-      element:<Main />,
-      children:[
+      path: '/',
+      element: <Main />,
+      children: [
         {
-          path:'/',
-          element:<Home />,
+          path: '/',
+          element: <Home />,
         },
         {
-          path:'/About',
-          element:<AboutUs />
-        },{
-          path:'/Contact',
-          element:<Contact />
+          path: '/About',
+          element: <AboutUs />
+        },
+        {
+          path: '/Contact',
+          element: <Contact />
         }
       ]
     }
-  ])
+  ]);
+
   return (
     <div className="App">
-      <RouterProvider router={routes} />
+      <DarkModeProvider>
+        <RouterProvider router={routes} />
+      </DarkModeProvider>
     </div>
   );
 }
