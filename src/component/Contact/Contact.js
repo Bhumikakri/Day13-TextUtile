1606a19ba47a6611154f54d30b5bcac182e11b13
Bhumika Kumari <128302166+Bhumikakri@users.noreply.github.com>
2024-01-18 01:17:59 +0530

    About section is left

--- src/component/Contact/Contact.js
+++ src/component/Contact/Contact.js
@@ -1,7 +1,12 @@
+import './contact.css';
+
 const Contact=()=>{
     return(
         <div className="Contact">
-            <h1>Contact Section here</h1>
+            <h1>Contact Me</h1>
+            <p>Created by : Bhumika Kumari</p>
+            <p>Email Address : bhumikakri2002@gmail.com</p>
+            <p>Github link : https://github.com/Bhumikakri</p>
         </div>
 
     );
